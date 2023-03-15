6b895347c4e3f2028e1ccf81365b034234fc7064
DarkLightWindraK <DarkLightWindraK@yandex.ru>
2023-03-15 13:20:52 +0300

    Final version

--- Labwork2OOD/FileHandler.swift
+++ Labwork2OOD/FileHandler.swift
@@ -4,9 +4,10 @@ protocol FileHandler: AnyObject {
     var pathsToRead: [String] { get set }
     var pathToWrite: String { get set }
     
+    init?(paths: [String])
+    
     func readFile(path: String)
     func writeFile(path: String)
-    init?(paths: [String])
 }
 
 extension FileHandler {
@@ -43,4 +44,53 @@ class FirstFileHandler: FileHandler {
         
         run()
     }
+}
+
+class SecondFileHandler: FileHandler {
+    var pathsToRead: [String] = []
+    var pathToWrite: String
+    
+    private var words = Set<String>()
+    private var resultPaths: [String] = []
+    
+    func readFile(path: String) {
+        var mySet: Set<String> = []
+        _ = try? String(
+            contentsOf: URL(filePath: path),
+            encoding: .utf8
+        )
+            .components(separatedBy: CharacterSet(charactersIn: " \n"))
+            .filter { !$0.isEmpty }
+            .map { mySet.insert(String($0)) }
+        if words.isSubset(of: mySet) {
+            resultPaths.append(path)
+        }
+    }
+    
+    func writeFile(path: String) {
+        resultPaths.forEach { resultPath in
+            try? resultPath.write(toFile: path, atomically: false, encoding: .utf8)
+        }
+    }
+    
+    required init?(paths: [String]) {
+        guard paths.count >= 3 else { return nil }
+        
+        pathToWrite = paths.last!
+        _ = try? String(
+            contentsOf: URL(filePath: paths.first!),
+            encoding: .utf8
+        )
+            .components(separatedBy: CharacterSet(charactersIn: " \n"))
+            .filter { !$0.isEmpty }
+            .map { words.insert($0) }
+        
+        for index in 1..<paths.count - 1 {
+            pathsToRead.append(paths[index])
+        }
+        
+        run()
+    }
+    
+    
 }
--- Labwork2OOD/main.swift
+++ Labwork2OOD/main.swift
@@ -1,3 +1,40 @@
 import Foundation
 
-_ = FirstFileHandler(paths: ["/Users/smirnoff/Desktop/Text1.txt", "/Users/smirnoff/Desktop/Text2.txt"])
+var task: Int?
+repeat {
+    print("Выберите задачу: 1 или 2")
+    task = Int(readLine() ?? "")
+} while task == nil
+
+switch task {
+case 1:
+    var numberOfPaths: Int?
+    repeat {
+        print("Введите количество путей (не менее 2): ")
+        numberOfPaths = Int(readLine() ?? "")
+    } while numberOfPaths == nil
+    
+    var paths: [String] = []
+    
+    for _ in 1...numberOfPaths! {
+        paths.append(readLine() ?? "")
+    }
+    
+    _ = FirstFileHandler(paths: paths)
+case 2:
+    var numberOfPaths: Int?
+    repeat {
+        print("Введите количество путей (не менее 3): ")
+        numberOfPaths = Int(readLine() ?? "")
+    } while numberOfPaths == nil
+    
+    var paths: [String] = []
+    
+    for _ in 1...numberOfPaths! {
+        paths.append(readLine() ?? "")
+    }
+    
+    _ = SecondFileHandler(paths: paths)
+default:
+    break
+}
